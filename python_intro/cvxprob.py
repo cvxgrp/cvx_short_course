98bd792e8f25bb756c3ea0a70a6fc6b861aae9d9
arlenk <arlen.khodadadi@gmail.com>
2018-10-29 22:32:53 -0400

    update python_intro for cvxpy 1.0

--- python_intro/cvxprob.py
+++ python_intro/cvxprob.py
@@ -1,18 +1,18 @@
-from cvxpy import *
+import cvxpy as cvx
 import numpy as np
 
 # Problem data.
 m = 30
 n = 20
 np.random.seed(1)
-A = np.asmatrix(np.random.randn(m, n))
-b = np.asmatrix(np.random.randn(m,1))
+A = np.random.randn(m, n)
+b = np.random.randn(m)
 
 # Construct the problem.
-x = Variable(n)
+x = cvx.Variable(n)
 # *, +, -, / are overloaded to construct CVXPY objects.
-cost = sum_squares(A*x - b)
-objective = Minimize(cost)
+cost = cvx.sum_squares(A*x - b)
+objective = cvx.Minimize(cost)
 # <=, >=, == are overloaded to construct CVXPY constraints.
 constraints = [0 <= x, x <= 1]
 prob = Problem(objective, constraints)
@@ -20,7 +20,7 @@ prob = Problem(objective, constraints)
 # The optimal objective is returned by prob.solve().
 result = prob.solve()
 # The optimal value for x is stored in x.value.
-print x.value
+print(x.value)
 # The optimal Lagrange multiplier for a constraint
 # is stored in constraint.dual_value.
-print constraints[0].dual_value
+print(constraints[0].dual_value)
